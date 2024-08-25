cf5410f15e70017da67c85ffe9ea139cb1553ee9
chinmaykd21 <chinmaykd21@gmail.com>
2024-08-25 19:14:04 -0400

    add helpful headers

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -5,17 +5,33 @@ const leakyBucket = new LeakyBucket(5, 60 * 1000); // 3 requests per minute
 
 export async function GET(req: NextRequest) {
   // Provided by vercel
-  const ip = req.ip!;
+  const ip = req.ip ? req.ip : req.headers.get("x-forward-for");
 
-  if (!leakyBucket.addRequest(ip)) {
-    return NextResponse.json({
-      error: "Too many requests. Please try again after some time!",
-      message: "",
-    });
+  if (!leakyBucket.addRequest(ip!)) {
+    return NextResponse.json(
+      {
+        error: "Too many requests. Please try again after some time!",
+        message: "",
+      },
+      {
+        status: 429,
+        headers: {
+          "Retry-After": "60", // Retry after 60 seconds
+          "X-RateLimit-Limit": "5", // Maximum requests allowed
+          "X-RateLimit-Remaining": "0", // Remaining requests in the current window
+          "X-RateLimit-Reset": `${Math.floor(Date.now() / 1000) + 60}`, // Time when the rate limit will reset (Unix timestamp)
+        },
+      }
+    );
   }
 
-  return NextResponse.json({
-    message: "Hello World!",
-    error: "",
-  });
+  return NextResponse.json(
+    {
+      message: "Hello World!",
+      error: "",
+    },
+    {
+      status: 200,
+    }
+  );
 }
