890da48613cbc2b960a7f188b6cfac5ae7423eb6
chinmaykd21 <chinmaykd21@gmail.com>
2024-08-25 20:49:19 -0400

    handle local host

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -4,22 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 const leakyBucket = new LeakyBucket(5, 60 * 1000); // 3 requests per minute
 
 export async function GET(req: NextRequest) {
-  // Provided by vercel
-  const ip = req.ip ? req.ip : req.headers.get("x-forward-for");
+  // Retrieve the client's IP address from the request object
+  const ip =
+    req.ip ||
+    req.headers.get("x-forwarded-for") ||
+    req.headers.get("x-real-ip") ||
+    "127.0.0.1"; // Fallback to localhost
 
+  // If the IP is still undefined, we fallback to 'unknown'
   if (!ip) {
     return NextResponse.json(
       {
-        error: "IP not found!!!",
+        error: "Unable to determine IP address.",
         message: "",
       },
       {
-        status: 400,
+        status: 400, // Bad request since IP is required
       }
     );
   }
 
-  if (!leakyBucket.addRequest(ip)) {
+  if (!leakyBucket.addRequest(ip as string)) {
     return NextResponse.json(
       {
         error: "Too many requests. Please try again after some time!",
